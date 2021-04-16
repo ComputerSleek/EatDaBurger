9f28eb83b34fe2996f35741934ac059d0a3f6dd9
Trill Ville <computersleek@gmail.com>
2021-04-16 17:54:46 -0400

    update server

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,7 +3,7 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model to use its database functions.
-var burger = require("./models/burgers.js");
+var burger = require("../models/burgers.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
@@ -17,7 +17,7 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/burgers", function(req, res) {
-  burgers.insertOne ([
+  burger.insertOne ([
     "name", "devoured"
   ], [
     req.body.name, req.body.devoured
@@ -32,7 +32,7 @@ router.put("/api/burgers/:id", function(req, res) {
 
   console.log("condition", condition);
 
-  burgers.update({
+  burger.updateOne ({
     devoured: req.body.devoured
   }, condition, function(result) {
     if (result.changedRows == 0) {
@@ -44,11 +44,11 @@ router.put("/api/burgers/:id", function(req, res) {
   });
 });
  
-// to call the delete key of the cat model, and to pass in arguments as necessary
+// to call the delete key of the model, and to pass in arguments as necessary
 router.delete("/api/burgers/:id", (req, res) => {
   // req.params --> we have req.params.id
   // req.body --> not needed
-  // query.... no do we have model? --> we will use the cat.delete
+  // query.... no do we have model? --> we will use the .delete
   burger.delete({ id: req.params.id }, data => {
     // errs -> no error input
     // data hande it
---
+++ public/assets/js/burger.js
@@ -0,0 +1,61 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+    $(".delete-button").on("click", function(event) {
+      console.log("Delete worked");
+      console.log(this);
+      var button = $(this);
+      // grabs id from button
+      var id = button.attr("data-id");
+      $.ajax("/api/burgers/" + id, {
+        type: "DELETE"
+      }).then(
+        function(results) {
+          console.log(results);
+          location.reload();
+        }
+      );
+    });
+    $(".change-devoured").on("click", function(event) {
+      var id = $(this).data("id");
+      var newDevoured = $(this).data("newdevoured");
+  
+      var newDevouredState = {
+        devoured: 1
+      };
+  
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: newDevouredState
+      }).then(
+        function() {
+          console.log("changed devoured to", newDevoured);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newBurger = {
+        name: $("#ca").val().trim(),
+        devoured: $("[name=devoured]:checked").val().trim()
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("created new burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  });
+  
